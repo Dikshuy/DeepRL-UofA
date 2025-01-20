ea9bfcd15a0dfb06c2e54ca0b28de3352a76a5b4
Dikshuy <dikshant1212hsr@gmail.com>
2025-01-19 20:50:30 -0700

    assigment 1 done

---
+++ assignment1/a1/q_learning.py
@@ -0,0 +1,47 @@
+import numpy as np
+
+class QLearning:
+    """Class that implements Q-Learning."""
+
+    def __init__(self,
+                 num_states,
+                 num_actions,
+                 step_size,
+                 explorer,
+                 discount=0.99):
+        self.explorer = explorer
+        self.step_size = step_size
+        self.q = np.zeros((num_states, num_actions))
+        self.discount = discount
+        # Your code here: introduce any variables you may need
+        self.prev_state = None
+        self.prev_action = None
+        # End your code here
+
+    def update_q(self, obs, action, reward, next_obs, terminated):
+        # start your code
+        self.q[obs, action] += self.step_size * (reward + self.discount * (1 - terminated) * np.max(self.q[next_obs]) - self.q[obs, action])
+        # end your code 
+    
+    def act(self, obs: int) -> int:
+        """Returns an integer 
+        """
+        # Your code here
+        self.prev_state = obs
+        action = self.explorer.select_action(self.q[obs])
+        self.prev_action = action
+        # End your code here
+        return action
+        
+    def process_transition(self, obs: int, reward: float, terminated: bool, truncated: bool) -> None:
+        """Observe consequences of the last action and update estimates accordingly.
+
+        Returns:
+            None
+        """
+        # Your code here
+        state = self.prev_state
+        action = self.prev_action
+        next_state = obs
+        self.update_q(state, action, reward, next_state, terminated) # keep this line
+        # End your code here
---
+++ assignment1/a1_partial_mark/agent_environment.py
@@ -0,0 +1,36 @@
+def agent_environment_episode_loop(agent, env, num_episodes):
+    episode_returns = []
+    for episode in range(num_episodes):
+        observation, info = env.reset()
+        # start your code
+        episode_return = 0
+        done = False
+        while not done:
+            action = agent.act(observation)
+            next_observation, reward, terminated, truncated, info = env.step(action)
+            agent.process_transition(next_observation, reward, terminated, truncated)
+            observation = next_observation
+            episode_return += reward
+            done = terminated or truncated
+        episode_returns.append(episode_return)
+        # end your code
+    return episode_returns
+
+def agent_environment_step_loop(agent, env, num_steps):
+    observation, info = env.reset()
+    episode_returns = []
+    episode_return = 0
+    for _ in range(num_steps):
+        # start your code
+        action = agent.act(observation)
+        next_observation, reward, terminated, truncated, info = env.step(action)
+        agent.process_transition(next_observation, reward, terminated, truncated)
+        observation = next_observation
+        episode_return += reward
+        done = terminated or truncated
+        if done:
+            episode_returns.append(episode_return)
+            episode_return = 0
+            observation, info = env.reset()
+        # end your code
+    return episode_returns
