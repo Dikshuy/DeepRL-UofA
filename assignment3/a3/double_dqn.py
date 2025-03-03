2cd71b8d380171d8d30b8315f4d06a99b086ea3a
Dikshuy <dikshant1212hsr@gmail.com>
2025-03-02 21:20:52 -0700

    added double dqn part

--- assignment3/a3/double_dqn.py
+++ assignment3/a3/double_dqn.py
@@ -5,5 +5,11 @@ class DoubleDQN(dqn.DQN):
 
     def compute_targets(self, batched_rewards, batched_next_states, batched_discounts, batch_terminated):
         # begin your code
-        pass
+        with torch.no_grad():
+            q_values = self.target_network(batched_next_states)
+            q_values = q_values.max(dim=1)[0]
+            q_values = q_values * batched_discounts
+            q_values = q_values * (1 - batch_terminated)
+            q_values = q_values + batched_rewards
+        return q_values
         # end your code
