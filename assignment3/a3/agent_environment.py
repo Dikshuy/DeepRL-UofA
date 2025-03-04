c21613b2e9c73fc7cf642681d2fcadb3b8c28d88
Dikshuy <dikshant1212hsr@gmail.com>
2025-03-03 19:32:49 -0700

    agent env ready

--- assignment3/a3/agent_environment.py
+++ assignment3/a3/agent_environment.py
@@ -9,7 +9,24 @@ def agent_environment_episode_loop(agent, env, num_episodes, debug=False, track_
             episode_q_values = []
         observation, info = env.reset()
         # start your code
-        pass
+        done = False
+        episode_return = 0
+        while not done:
+            action = agent.act(observation)
+            if track_q:
+                obs_tensor = torch.tensor(observation, dtype=torch.float32)
+                q_values = agent.q_network(obs_tensor)
+                episode_q_values.append(q_values[action].item())
+            next_observation, reward, terminated, truncated, info = env.step(action)
+            agent.process_transition(next_observation, reward, terminated, truncated)
+            observation = next_observation
+            episode_return += reward
+            done = terminated or truncated
+        episode_returns.append(episode_return)
+        if track_q:
+            mean_q_predictions.append(np.mean(episode_q_values))
+        if debug:
+            print(f"Episode {episode} return: {episode_return}")
         # end your code
     if track_q:
         return episode_returns, mean_q_predictions
