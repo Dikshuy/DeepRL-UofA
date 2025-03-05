5a21fc37dcff222a8c60962121c3e8019dbfa474
Dikshuy <dikshant1212hsr@gmail.com>
2025-03-05 01:21:36 -0700

    reduce cpu and time

--- assignment3/a3/sweep.sh
+++ assignment3/a3/sweep.sh
@@ -23,8 +23,8 @@ for config in "${CONFIGS[@]}"; do
                             cat > "$script_name" << EOL
 #!/bin/bash
 #SBATCH --account=def-mtaylor3_cpu
-#SBATCH --mem-per-cpu=8G
-#SBATCH --time=60:00:00
+#SBATCH --mem-per-cpu=4G
+#SBATCH --time=07:00:00
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=1
 
